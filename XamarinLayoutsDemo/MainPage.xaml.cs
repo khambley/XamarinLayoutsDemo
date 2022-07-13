99d414596c58475a3c54d2edaf0e9f1a92dd4f38
Katherine Hambley <superdesigngirl@mac.com>
2022-07-13 15:23:59 -0500

    Added Span Layout Page and icons

--- XamarinLayoutsDemo/MainPage.xaml.cs
+++ XamarinLayoutsDemo/MainPage.xaml.cs
@@ -24,5 +24,25 @@ namespace XamarinLayoutsDemo
             await Navigation.PushAsync(new ScrollviewPage());
             
         }
+
+        public async void GridviewPageButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            // This is to make the Scrollview page the root page. (No back arrow in navbar)
+            //Application.Current.MainPage = new NavigationPage(new ScrollviewPage());
+
+            // This is to add the Scrollview page to the Navigation Stack (Adds a back arrow in navbar)
+            await Navigation.PushAsync(new GridviewPage());
+
+        }
+
+        public async void SpanGridPageButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            // This is to make the Scrollview page the root page. (No back arrow in navbar)
+            //Application.Current.MainPage = new NavigationPage(new ScrollviewPage());
+
+            // This is to add the Scrollview page to the Navigation Stack (Adds a back arrow in navbar)
+            await Navigation.PushAsync(new SpanGridPage());
+
+        }
     }
 }
